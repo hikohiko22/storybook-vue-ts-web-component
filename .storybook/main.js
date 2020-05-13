2adacc8338dfd9716958f05142d0e56c54983fd7
Mitsu Narita <naritamitsutaka@naritamitsutakanoMacBook-Pro.local>
2020-05-13 18:35:09 +0900

    addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/addon-knobs/register']
+  addons: ['@storybook/addon-knobs/register','@storybook/addon-actions']
 }
